60c7cdd2afe7eb9f9397073ec2599b09a4c9f45c
AhmedNasser-Front-End <ahmednasser_8@yahoo.com>
2020-06-23 02:06:00 +0200

    Updates

---
+++ precache-manifest.af1b4e17611d3400e3cf0fad97ccbf42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83d9cca97736387ee11ad61fb55130d",
+    "url": "/crud/index.html"
+  },
+  {
+    "revision": "50d7ed2cbb01a6fabaf8",
+    "url": "/crud/static/css/main.b8e12702.chunk.css"
+  },
+  {
+    "revision": "e18a021f0cfe06c08311",
+    "url": "/crud/static/js/2.6e7e55d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crud/static/js/2.6e7e55d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50d7ed2cbb01a6fabaf8",
+    "url": "/crud/static/js/main.d3cd2eb5.chunk.js"
+  },
+  {
+    "revision": "b4a282f0702dfebf023d",
+    "url": "/crud/static/js/runtime-main.b006720b.js"
+  }
+]);
